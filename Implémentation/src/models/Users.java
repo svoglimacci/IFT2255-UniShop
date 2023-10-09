60c3401b0f8e1a1a5003632d9ba36c96ff2c9af3
svoglimacci <44271817+svoglimacci@users.noreply.github.com>
2023-10-09 17:03:23 -0400

    feature: auth
    * added persistent login & registration

---
+++ Implémentation/src/models/Users.java
@@ -0,0 +1,27 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class Users {
+    @JsonProperty("buyers")
+    public final List<Buyer> buyers;
+    @JsonProperty("sellers")
+    public final List<Seller> sellers;
+
+    public Users(@JsonProperty("buyers") List<Buyer> buyers,
+                 @JsonProperty("sellers") List<Seller> sellers) {
+        this.buyers = buyers;
+        this.sellers = sellers;
+    }
+
+    public List<Buyer> getBuyers() {
+        return this.buyers;
+    }
+
+    public List<Seller> getSellers() {
+        return this.sellers;
+    }
+}
+
