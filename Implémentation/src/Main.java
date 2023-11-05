91f167aa9248a44312578f6168004608bb77df4a
svoglimacci <44271817+svoglimacci@users.noreply.github.com>
2023-11-05 09:28:17 -0500

    Feature/product list (#28)
    * added ProductList, Buyer MVC
    
    * added models, serialization, updated view
    
    * cleaned up BuyerView, toString function

--- Implémentation/src/Main.java
+++ Implémentation/src/Main.java
@@ -21,6 +21,6 @@ public class Main {
         AuthView authView = new AuthView();
         authView.start();
 
-
     }
+
 }
--- Implémentation/src/models/Seller.java
+++ Implémentation/src/models/Seller.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Seller extends User {
-    private String businessName;
+    private final String businessName;
 
     @JsonCreator
     public Seller(@JsonProperty("username") String username,
@@ -13,16 +13,6 @@ public class Seller extends User {
                   @JsonProperty("address") String address,
                   @JsonProperty("businessName") String businessName) {
         super(username, password, email, address);
-        this.businessName = businessName;
-    }
-
-    // Getter methods
-    public String getBusinessName() {
-        return this.businessName;
-    }
-
-    // Setter methods
-    public void setBusinessName(String businessName) {
         this.businessName = businessName;
     }
 
